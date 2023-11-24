e81cba9d1cec63230ee8e664aa4c6b54aecce1aa
Machuche Wambura <machuchesteven@gmail.com>
2023-11-24 19:01:53 +0300

    Mastering the basics

---
+++ basics/basics/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        static void main()
+        {
+            Console.WriteLine("Hello, World again!");
+        }
+        main();
+    }
+}
